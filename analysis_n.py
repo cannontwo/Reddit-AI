d8fb60afd73cf35ce7fc520b228c0c834677908e
Cannon Lewis <cannon10100@gmail.com>
2014-03-11 22:35:51 -0500

    Deleted some old stuff, added generalized markov chain prediction (SUUUUUUUPER slow)

--- analysis_n.py
+++ analysis_n.py
@@ -5,14 +5,10 @@ __author__ = 'cannon'
 import string
 import os
 import random
-import json
 
 
-def analyze():
+def analyze(order):
     already_processed = []
-    if os.path.isfile("already_processed2.json"):
-        with open("already_processed2.json", "r") as read_file:
-            already_processed = json.loads(read_file.read())
 
     titles = {}
 
@@ -25,9 +21,6 @@ def analyze():
                     if params[1] not in already_processed:
                         titles[params[1]] = params[0]
                         already_processed.append(params[1])
-
-    with open("already_processed2.json", "w") as write_file:
-        write_file.write(json.dumps(already_processed))
 
     titles_data = []
 
@@ -45,17 +38,15 @@ def analyze():
 
     combos = {}
 
-    if os.path.isfile("frequencies2.json"):
-        with open("frequencies2.json", "r") as read_file:
-            combos = json.loads(read_file.read())
-
     if len(titles_data) > 0:
         for title_data in titles_data:
             n += 1
             word_sum += title_data[0]
             char_sum += title_data[1]
             for x in range(0, len(title_data[2]) - 2):
-                combo = str(title_data[2][x]) + str(title_data[2][x+1])
+                combo = ""
+                for i in range(0, order):
+                    combo += str(title_data[2][x+i])
                 if combo not in combos:
                     combos[combo] = 1
                 else:
@@ -66,9 +57,6 @@ def analyze():
 
         print "Word length Average: %s" % word_avg
         print "Character length Average: %s" % char_avg
-
-    with open("frequencies2.json", "w") as write_file:
-        write_file.write(json.dumps(combos))
 
     probs = {}
 
@@ -81,11 +69,11 @@ def analyze():
 
     return_string = ""
     while len(return_string) < 40:
-        if len(return_string) > 2:
-            last_one = return_string[-1:]
+        if len(return_string) > order:
+            last_one = return_string[-(order - 1):]
             sub_prob = {}
             for key, value in probs.iteritems():
-                if key[0] is last_one:
+                if key[:-1] is last_one:
                     sub_prob[key] = value
 
             sub_prob_total = sum(sub_prob.values())
@@ -94,7 +82,7 @@ def analyze():
             for key, value in sub_prob.iteritems():
                 s += value
                 if s >= r:
-                    return_string += key[1]
+                    return_string += key[order - 1]
                     break
         else:
             r, s = random.random(), 0
@@ -105,3 +93,6 @@ def analyze():
                     break
 
     return return_string
+
+if __name__ == "__main__":
+    print analyze(3)
